75a8c19d4663e339ccd93f4101c1a26e81e60955
root <fabioamedeiro@gmail.com>
2017-08-08 23:32:58 +0100

    adding functions read, inserting and updating of db

---
+++ database.sql
@@ -0,0 +1,24 @@
+create database journal2;
+create user 'journal'@'localhost' IDENTIFIED BY 'journal123';
+Grant ALL privileges on  journal2.* TO 'journal'@'localhost';
+use journal2;
+create table blueface(
+	       id  varchar(32) NOT NULL,
+	       nphone varchar(20) NOT NULL,
+	       voicemail  varchar(20) NOT NULL,
+	       PRIMARY KEY (id)
+);
+
+
+create table office(
+       mac varchar(32) NOT NULL,
+       id  varchar(32) NOT NULL,
+       password varchar(32) NOT NULL,
+       users varchar(20) NOT NULL,
+       location varchar(20) NOT NULL,
+       status  varchar(20) NOT NULL,
+       PRIMARY KEY (mac)
+       FOREIGN KEY (id) references blueface(id)
+);
+       
+       
